ca5a209ee5d89b23bc4932abf08d157d43b228a9
Michel NAUD <michel.naud26400@gmail.com>
2021-03-14 14:37:47 +0100

    add filtering on attendance heatmap, navigation in 24 hours history, close #222

--- src/Perun/Service/LogStatService.php
+++ src/Perun/Service/LogStatService.php
@@ -2,6 +2,7 @@
 
 namespace App\Perun\Service;
 
+use App\Entity\User;
 use App\Perun\DTO\LogStatHourly;
 use App\Perun\Entity\Instance;
 use App\Perun\Entity\LogStat;
@@ -22,16 +23,24 @@ class LogStatService
     /**
      * @return LogStatHourly[]|array
      */
-    public function loadAttendanceHourly(Instance $instance = null): array
+    public function loadAttendanceHourly(Instance $instance = null, \DateTime $from, \DateTime $to = null): array
     {
-        $now = new \DateTime();
+        $safeDelay = 4;
+        $startSafe = new \DateTime((clone $from)->modify(sprintf('-%d hours', $safeDelay))->format('Y-m-d H:0'));
+        if (null === $to) {
+            $endSafe = new \DateTime((new \DateTime('now'))->format('Y-m-d H:59:59'));
+        } else {
+            $endSafe = new \DateTime($to->format('Y-m-d H:59:59'));
+        }
 
         $query = $this->entityManager->getRepository(LogStat::class)->createQueryBuilder('stat')
             ->leftJoin('stat.player', 'player')
             ->leftJoin('player.user', 'user')
             ->select('stat,player,user')
             ->andWhere('stat.datetime >= :start')
-            ->setParameter('start', (new \DateTime('now'))->modify('-28 hours')); // stats are recorded at the end of the player session
+            ->setParameter('start', $startSafe) // stats are recorded at the end of the player session
+            ->andWhere('stat.datetime <= :end')
+            ->setParameter('end', $endSafe);
         if (null !== $instance) {
             $query->join('stat.mission', 'mission')
                 ->andWhere('mission.instance = :instance')
@@ -41,10 +50,13 @@ class LogStatService
         /** @var LogStat[] $entries */
         $entries = $query->getQuery()->getResult();
 
+        // count how many hours between from and to
+        $nbHours = ceil(($endSafe->getTimestamp() - $startSafe->getTimestamp()) / 3600) - $safeDelay;
+
         // prepare all records
         $results = [];
-        for ($hour = 24; $hour > 0; --$hour) {
-            $start = (clone $now)->modify(sprintf('-%d hours', $hour));
+        for ($hour = $nbHours; $hour > 0; --$hour) {
+            $start = (clone $to)->modify(sprintf('-%d hours', $hour - 1));
             $start->setTime($start->format('G'), 0, 0);
             $end = (clone $start)->modify('+1 hour -1 second');
 
@@ -61,7 +73,12 @@ class LogStatService
         $countMap = [];
 
         foreach ($entries as $entry) {
-            for ($hour = 0; $hour <= $entry->getTime() / 60; ++$hour) {
+            $entryHours = ceil($entry->getTime() / 60);
+            // session on 2 different hours ?
+            if ($entry->getDatetime()->format('i') - $entry->getTime() % 60 < 0) {
+                $entryHours++;
+            }
+            for ($hour = 0; $hour < $entryHours; ++$hour) {
                 $playerStartHour = (clone $entry->getDatetime())->modify(sprintf('-%d minutes +%d hours', $entry->getTime(), $hour))->format('Y-m-d H');
                 if (isset($results[$playerStartHour])) {
                     /** @var LogStatHourly $stat */
@@ -126,9 +143,16 @@ class LogStatService
         return $categories;
     }
 
-    public function getAttendanceChart(string $chartName, Instance $instance = null): Highchart
+    public function getAttendanceChart(string $chartName, Instance $instance = null, \DateTime $from = null, \DateTime $to = null): Highchart
     {
-        $stats = $this->loadAttendanceHourly($instance);
+        if (null === $to) {
+            $to = new \DateTime('now');
+        }
+        if (null === $from) {
+            $from = (clone $to)->modify('-24 hours');
+        }
+
+        $stats = $this->loadAttendanceHourly($instance, $from, $to);
 
         $history = new Highchart();
         $history->chart->renderTo($chartName);  // The #id of the div where to render the chart
@@ -142,7 +166,14 @@ class LogStatService
         } else {
             $history->title->text('Fréquentation du serveur');
         }
-        $history->xAxis->title(['text' => 'Historique des 24 dernières heures']);
+        $title = 'Historique par heure';
+        if ($to->format('Y-m-d') !== (new \DateTime('now'))->format('Y-m-d')) {
+            $title .= sprintf(' - du %s au %s', $from->format('d/m/Y'), $to->format('d/m/Y'));
+        }
+        else {
+            $title .= ' - aujourd\'hui';
+        }
+        $history->xAxis->title(['text' => $title]);
         $history->yAxis->title(['text' => 'Joueurs connectés']);
         $history->plotOptions->column(['stacking' => 'normal']);
         $history->xAxis->categories($this->getCategoriesFromLogStatHourly($stats));
@@ -151,7 +182,7 @@ class LogStatService
         return $history;
     }
 
-    public function loadAttendanceHeatmapHourly(Instance $instance = null, int $weeks): array
+    public function loadAttendanceHeatmapHourly(Instance $instance = null, int $weeks, string $filter): array
     {
         $now = new \DateTime();
         $periodEnd = (clone $now)->modify('-1 day')->setTime(23, 59, 59);
@@ -170,6 +201,26 @@ class LogStatService
             $query->join('stat.mission', 'mission')
                 ->andWhere('mission.instance = :instance')
                 ->setParameter('instance', $instance);
+        }
+
+        // add filtering on user status
+        if (in_array($filter, ['members', 'cadets', 'cadets-members'])) {
+            $query->join('player.user', 'user')
+                ->andWhere('user.status IN (:statuses)');
+        }
+        switch ($filter) {
+            case 'members':
+                $query->setParameter('statuses', User::STATUSES_MEMBER);
+                break;
+            case 'cadets':
+                $query->setParameter('statuses', User::STATUS_CADET);
+                break;
+            case 'cadets-members':
+                $query->setParameter('statuses', array_merge(User::STATUSES_MEMBER, [User::STATUS_CADET]));
+                break;
+            case 'all':
+            default:
+                break;
         }
 
         $entries = $query->getQuery()->getArrayResult();
@@ -228,9 +279,9 @@ class LogStatService
         return $series;
     }
 
-    public function getHeatmapChart(string $chartName, Instance $instance = null, int $weeks = 2): Highchart
+    public function getHeatmapChart(string $chartName, Instance $instance = null, int $weeks = 2, string $filter = 'all'): Highchart
     {
-        $stats = $this->loadAttendanceHeatmapHourly($instance, $weeks);
+        $stats = $this->loadAttendanceHeatmapHourly($instance, $weeks, $filter);
 
         $heatmap = new Highchart();
         $heatmap->chart->renderTo($chartName);  // The #id of the div where to render the chart
