4007dd2aefde594f43088ce0fd973b921e317b22
Michel NAUD <michel.naud26400@gmail.com>
2025-12-24 14:03:50 +0100

    add icons

--- src/Controller/DcsBotController.php
+++ src/Controller/DcsBotController.php
@@ -35,4 +35,31 @@ class DcsBotController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/{serverName}", name="dcsbot_server")
+     */
+    public function server(string $serverName, InfoApi $infoApi): Response
+    {
+        $server = null;
+        $serverStats = null;
+        $error = null;
+
+        try {
+            $servers = $infoApi->serversServerapiServersGet($serverName);
+            if (empty($servers)) {
+                throw $this->createNotFoundException('Serveur non trouvé');
+            }
+            $server = $servers[0];
+            $serverStats = $infoApi->serverstatsServerapiServerstatsGet($serverName);
+        } catch (ApiException $e) {
+            $error = 'Impossible de contacter le service DCSServerBot: '.$e->getMessage();
+        }
+
+        return $this->render('dcsbot/server.html.twig', [
+            'server' => $server,
+            'serverStats' => $serverStats,
+            'error' => $error,
+        ]);
+    }
 }
